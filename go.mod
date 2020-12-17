4636f2ae9974f449c0b50ef6e224bf1a12713357
thuonghh <thuonghh@vng.com.vn>
2020-12-17 16:07:57 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/headend/agent-control-service
+
+go 1.13
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"log"
+
+)
+
+
+func main()  {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	log.Println("Agent control service")
+}
