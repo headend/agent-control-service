ab29fd5b31be515ff28d2ec91dd774c8269d4e3a
thuonghh <thuonghh@vng.com.vn>
2020-12-17 20:26:23 +0700

    test

--- control-services/control-services.go
+++ control-services/control-services.go
@@ -2,13 +2,29 @@ package control_services
 
 import (
 	"github.com/headend/share-module/configuration"
-	"github.com/headend/iptv-agentd-control-service/proto"
+	agentctlpb "github.com/headend/agent-control-service/proto"
+	"google.golang.org/grpc"
+	"log"
+	"net"
 )
 
 func StartServer()  {
 	var config configuration.Conf
-	if confFile !=nil {
-		config.ConfigureFile = *confFile
-	}
+
 	config.LoadConf()
+	ln, err := net.Listen("tcp", "0.0.0.0:5000")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	rpcServer := grpc.NewServer()
+
+	if rpcServer == nil {
+		log.Fatalf("failed to register server: %v", err)
+	}
+	if err := rpcServer.Serve(ln); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+
+
 }
